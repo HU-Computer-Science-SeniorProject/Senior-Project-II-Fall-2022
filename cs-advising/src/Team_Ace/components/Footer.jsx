b8b6173640c6ef0b66d3a4b702181f32e2957566
Samuel Calmday <calmdaysamuel@gmail.com>
2022-10-28 17:28:36 -0400

    graduation home

--- cs-advising/src/Team_Ace/components/Footer.jsx
+++ cs-advising/src/Team_Ace/components/Footer.jsx
@@ -1,3 +1,11 @@
+import "./Footer.css";
+
 export function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer_main">
+      <a className="link">BISONWEB</a>
+      <a className="link">GET HELP</a>
+      <a className="link">CONTACT INFORMATION</a>
+    </div>
+  );
 }
--- cs-advising/src/Team_Ace/components/Header.jsx
+++ cs-advising/src/Team_Ace/components/Header.jsx
@@ -1,3 +1,37 @@
+import "./Header.css";
+
 export function Header() {
-  return <div>Header</div>;
+  const routes = [
+    { title: "Matriculation", link: "/matriculation" },
+    {
+      title: "Team 2",
+      link: "BD",
+    },
+    {
+      title: "Team 4",
+      link: "BD",
+    },
+    {
+      title: "Graduation",
+      link: "graduation",
+    },
+  ];
+  return (
+    <div className="header">
+      <div className="header_top">
+        <div className="howard_logo"></div>
+        <div className="header_title">Computer Science Advising</div>
+        <div className="headaer_login"></div>
+      </div>
+      <div className="header_bottom">
+        {routes.map((route) => {
+          return (
+            <a href={route.link} className="header_route">
+              {route.title}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
