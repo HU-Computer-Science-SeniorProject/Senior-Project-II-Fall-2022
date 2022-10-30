4ba556554ebdc934a82110512730e6c676dfa5e8
Ibukunoluwa Esan <esanibukun@gmail.com>
2022-10-30 18:48:02 -0400

    Add routes  for graduation application and checklist

--- cs-advising/src/App.js
+++ cs-advising/src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
 import { GraduationHome } from "./Team_Ace/@calmdaysamuel/GraduationHome";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { GraduationApplication } from "./Team_Ace/@calmdaysamuel/GraduationApplication";
+import { GraduationChecklist } from "./Team_Ace/@dInquisitor/GraduationChecklist";
 
 const router = createBrowserRouter([
   {
@@ -14,12 +13,18 @@ const router = createBrowserRouter([
     path: "/graduation",
     element: <GraduationHome />,
   },
+  {
+    path: "/graduation-application",
+    element: <GraduationApplication />,
+  },
+  {
+    path: "/graduation-checklist",
+    element: <GraduationChecklist />,
+  },
 ]);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
