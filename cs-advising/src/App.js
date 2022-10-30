7b6e3cf637e9698fb886b333919685f3cc479758
Ibukunoluwa Esan <esanibukun@gmail.com>
2022-10-30 18:44:47 -0400

    Add projectwide routing

--- cs-advising/src/App.js
+++ cs-advising/src/App.js
@@ -2,27 +2,25 @@ import logo from "./logo.svg";
 import "./App.css";
 import { GraduationAppliation } from "./Team_Ace/@calmdaysamuel/GraduationApplication";
 import { GraduationHome } from "./Team_Ace/@calmdaysamuel/GraduationHome";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <GraduationHome />,
+  },
+  {
+    path: "/graduation",
+    element: <GraduationHome />,
+  },
+]);
 
 function App() {
   return (
-    // <GraduationAppliation />
-    <GraduationHome />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <RouterProvider router={router} />
   );
 }
 
