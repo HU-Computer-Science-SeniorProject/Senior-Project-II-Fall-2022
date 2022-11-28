e96825eee4dec08c0100fa5aea66804141fdde4d
loosesoup <avonabrahamsjr@live.com>
2022-11-28 11:39:36 -0500

    clearer ui + hidden features needing to style

--- cs-advising/src/Team MVP/AlumniCommunication.jsx
+++ cs-advising/src/Team MVP/AlumniCommunication.jsx
@@ -18,15 +18,26 @@ export function AlumniCommunication() {
             <div className="main_gh_title">Alumni Communication</div>
             <div>
               <Button onClick={() => openInNewTab('https://forms.gle/WCwereQGxiAvd2z1A')}> 
-                Sign Up For Notifications!
+                Sign Up For Notifications! *link to google form*
               </Button>
             </div>
-            <div>
+            {/* <div display="flex" float="right" position="absolute">
+              <iframe 
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/cly-O_Ccv0A"
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+               </iframe>
+            </div> */}
+            <div display="flex" float="left" position="relative">
               <TwitterTimelineEmbed
                 sourceType="profile"
                 screenName="Howard_CEA"
                 options={{height: 400,
-                      width: 300}}
+                      width: 500}}
               />
             </div>
           </div>
