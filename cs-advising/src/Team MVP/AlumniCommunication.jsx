a52b50cc211b1f11cf6a153ff4a448b18c2cd142
loosesoup <avonabrahamsjr@live.com>
2022-11-28 10:22:05 -0500

    alumni comm update nov28

--- cs-advising/src/Team MVP/AlumniCommunication.jsx
+++ cs-advising/src/Team MVP/AlumniCommunication.jsx
@@ -1,10 +1,14 @@
 import { Header } from "../Team_Ace/components/Header";
 import { Footer } from "../Team_Ace/components/Footer";
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+import { Button } from '@mui/material';
 
 
 export function AlumniCommunication() {
 
-
+  const openInNewTab = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
 
 
     return (
@@ -12,8 +16,22 @@ export function AlumniCommunication() {
           <Header />
           <div className="gh_body">
             <div className="main_gh_title">Alumni Communication</div>
+            <div>
+              <Button onClick={() => openInNewTab('https://forms.gle/WCwereQGxiAvd2z1A')}> 
+                Sign Up For Notifications!
+              </Button>
+            </div>
+            <div>
+              <TwitterTimelineEmbed
+                sourceType="profile"
+                screenName="Howard_CEA"
+                options={{height: 400,
+                      width: 300}}
+              />
+            </div>
           </div>
           <Footer />
         </div>
       );
+      
 }
