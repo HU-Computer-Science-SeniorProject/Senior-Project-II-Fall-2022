225e1066bc3b725ab1bd2e817c7110aea1dbe4b8
“Nia-A” <nia.anderson1@bison.howard.edu>
2022-11-28 00:14:38 -0500

    fixed spelling error

--- cs-advising/src/Team MVP/ConnectAlumni.jsx
+++ cs-advising/src/Team MVP/ConnectAlumni.jsx
@@ -10,7 +10,7 @@ export function ConnectAlumni() {
           <Header />
           <div className="gh_body">
             <div className="main_gh_title">Connect With Alumni</div>
-            <h3>Harry Keeling Linkden</h3>
+            <h3>Harry Keeling LinkedIn</h3>
             <iframe src="https://www.linkedin.com/in/harry-keeling-235b1918/" height="200" width="300" title="Harry Keeling LinkedIn"></iframe>
           </div>
           <Footer />
