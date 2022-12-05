4d8f165b1881b7ab2a50dd63e51d4d5072d77aca
Javid Mitchell <98503376+javidt16@users.noreply.github.com>
2022-12-04 19:24:24 -0500

    Removed duplicate react app and merged login with main app

--- cs-advising/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ cs-advising/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
     "auth": {
-        "seniorproject2": {
+        "seniorproject": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
             "UserPoolId": "string",
--- cs-advising/src/index.js
+++ cs-advising/src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import '@aws-amplify/ui-react/styles.css'
 
-// import Amplify from 'aws-amplify'
-// import config from './aws-exports'
-// Amplify.configure(config)
+import {Amplify} from "aws-amplify";
+import config from './aws-exports'
+import { AmplifyProvider} from "@aws-amplify/ui-react";
+
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <AmplifyProvider>
     <App />
-  </React.StrictMode>
+  </AmplifyProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
