430c2e800673773c7f3c98a8f1491315399ac677
cryptonon <aayushphuyal0074@gmail.com>
2022-12-09 15:02:03 -0500

    updated admin Graduation Status view

--- cs-advising/src/Team_Ace/@aayush/AdvisorView.jsx
+++ cs-advising/src/Team_Ace/@aayush/AdvisorView.jsx
@@ -2,7 +2,10 @@ import { GraduationEligibleGrid } from './GraduationEligibleGrid';
 
 const rows = [
   { id: '@12345678', name: 'John Doe', entryYear: '2019 Fall', cumGPA: 4.0, majorGPA: 4.0, numDs: 0, comments: 'Summa Cum Laude' },
-  { id: '@23456789', name: 'John Doe', entryYear: '2019 Spring', cumGPA: 3.85, majorGPA: 3.9, numDs: 0, comments: 'Summa Cum Laude' },
+  { id: '@23456789', name: 'Doe Smith', entryYear: '2019 Spring', cumGPA: 3.15, majorGPA: 3.9, numDs: 0, comments: 'Summa Cum Laude' },
+  { id: '@34567890', name: 'Carter Doe', entryYear: '2019 Fall', cumGPA: 3.2, majorGPA: 3.8, numDs: 0, comments: 'Summa Cum Laude' },
+  { id: '@45678901', name: 'Maine Doe', entryYear: '2019 Spring', cumGPA: 3.8, majorGPA: 3.7, numDs: 0, comments: 'Summa Cum Laude' },
+  { id: '@56789012', name: 'Carry Doe', entryYear: '2019 Fall', cumGPA: 3.75, majorGPA: 3.95, numDs: 0, comments: 'Summa Cum Laude' },
 ];
 
 export function AdvisorView() {
