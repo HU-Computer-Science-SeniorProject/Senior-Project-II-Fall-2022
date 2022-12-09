7d2ddba2a8408b127abdabe99f07f87e6fe2ce11
cryptonon <aayushphuyal0074@gmail.com>
2022-12-09 14:42:14 -0500

    update student's graduation status view

--- cs-advising/src/Team_Ace/@aayush/GraduationStatus.jsx
+++ cs-advising/src/Team_Ace/@aayush/GraduationStatus.jsx
@@ -1,6 +1,5 @@
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
-import { AdvisorView } from "./AdvisorView";
 import { StudentView } from "./StudentView";
 
 export function GraduationStatus({ isAdvisor, student }) {
@@ -8,8 +7,6 @@ export function GraduationStatus({ isAdvisor, student }) {
     <div>
       <Header />
       <StudentView student={student} />
-      {/* {isAdvisor && <AdvisorView />}
-      {!isAdvisor && student && <StudentView student={student} />} */}
       <Footer />
     </div>
   );
